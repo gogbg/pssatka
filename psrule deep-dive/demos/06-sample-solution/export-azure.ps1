ab236f02c21eb83cca81553ac500aa5101c6b2dd
Georgi Ivanov <georgimilchevivanov@kpmg.com>
2024-11-30 12:17:36 +0200

    adjust ppt

--- psrule deep-dive/demos/06-sample-solution/export-azure.ps1
+++
@@ -1,6 +0,0 @@
-$azureDataFolder = Join-Path -Path $PSScriptRoot -ChildPath 'export' -AdditionalChildPath 'azure'
-if (-not (Test-Path -Path $azureDataFolder))
-{
-    New-Item -Path $azureDataFolder -ItemType Directory
-}
-Export-AzRuleData -OutputPath $azureDataFolder -Subscription '88e64586-f51c-4e13-9307-c97f28464675' -Tenant '098ebab6-0ca3-4735-973c-7e8b14e101ac'
